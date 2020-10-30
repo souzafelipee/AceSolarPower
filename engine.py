f06d8168680596e0d9c20e394aab7607fcc6f692
Felipe Souza <osouzafelipe@gmail.com>
2020-10-30 08:43:01 -0400

    alterações importantes

--- engine.py
+++ engine.py
@@ -2,7 +2,7 @@ from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-dbString = getenv('POSTGRES_URI')
+dbString = getenv('DATABASE_URL')
 print(dbString)
 engine = create_engine(dbString, echo=True)
 Base = declarative_base()
